bc23dafe216b0bde9798edcc4eeafe17ce38f0c6
MEBARKI16 <medaminemebarki@gmail.com>
2024-09-18 16:46:27 +0100

    update

--- reduxConcepts/redux-demo.js
+++ reduxConcepts/redux-demo.js
@@ -4,7 +4,6 @@ const counterReducer = (state = {
     counter2: 0
 }, action) => {
  return { counter2: state.counter2 + 1 };
-    
 }
 const store = redux.createStore(counterReducer);
 
@@ -12,6 +11,8 @@ const Affiche = () => {
     const y = store.getState()
     console.log(y)
 }
-
 store.subscribe(Affiche)
 Affiche()
+store.dispatch({type : "increment"})
+store.dispatch({type : "increment"})
+
