fce77650ea8746e875213d9ca2d7863bf9cddaf2
MEBARKI16 <medaminemebarki@gmail.com>
2024-09-19 15:31:14 +0100

    using redux data in react components

--- ReactRedux/src/components/Counter.js
+++ ReactRedux/src/components/Counter.js
@@ -1,28 +1,16 @@
-import React, { useState, useEffect } from 'react';
 import classes from './Counter.module.css';
-import store from '../store';
-
+import { useSelector, useDispatch } from 'react-redux';
 const Counter = () => {
-  const [counter, setCounter] = useState(store.getState().counter);
-
-  useEffect(() => {
-    const unsubscribe = store.subscribe(() => {
-      setCounter(store.getState().counter);
-    });
-    return () => {
-      unsubscribe(); // Clean up the subscription when the component unmounts
-    };
-  }, []);
-
+  const x = useSelector(state => state.counter)
+  const dispatch = useDispatch()
   const toggleCounterHandler = () => {
-    store.dispatch({ type: 'increment' });
-    console.log("Counter incremented");
+    dispatch({ type: "increment" })
   };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      <div className={classes.value}>{x}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
