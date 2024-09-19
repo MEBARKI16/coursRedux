db3878279400fb5fd157e3fb0da757ef02dacc08
MEBARKI16 <medaminemebarki@gmail.com>
2024-09-19 14:58:23 +0100

    Providing the store

--- ReactRedux/src/components/Counter.js
+++ ReactRedux/src/components/Counter.js
@@ -1,12 +1,28 @@
+import React, { useState, useEffect } from 'react';
 import classes from './Counter.module.css';
+import store from '../store';
 
 const Counter = () => {
-  const toggleCounterHandler = () => { };
+  const [counter, setCounter] = useState(store.getState().counter);
+
+  useEffect(() => {
+    const unsubscribe = store.subscribe(() => {
+      setCounter(store.getState().counter);
+    });
+    return () => {
+      unsubscribe(); // Clean up the subscription when the component unmounts
+    };
+  }, []);
+
+  const toggleCounterHandler = () => {
+    store.dispatch({ type: 'increment' });
+    console.log("Counter incremented");
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- ReactRedux/src/store/index.js
+++ ReactRedux/src/store/index.js
@@ -1,12 +1,18 @@
-import redux from 'redux'
+import { createStore } from 'redux';
 
+const initialState = { counter: 0 };
 
-const StoreReducer = ({ value = { counter: 0 }, action }) => {
+const StoreReducer = (state = initialState, action) => {
     switch (action.type) {
-        case "increment": return { counter: value.counter + 1 };
-        case "decrement": return { counter: value.counter - 1 };
-        default: return value;
+        case "increment":
+            return { counter: state.counter + 1 };
+        case "decrement":
+            return { counter: state.counter - 1 };
+        default:
+            return state;
     }
-}
-const store = redux.createStore(StoreReducer);
+};
+
+const store = createStore(StoreReducer);
+
 export default store;
