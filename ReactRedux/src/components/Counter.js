263d1b774f28a1707877253d68a5c287de380862
MEBARKI16 <medaminemebarki@gmail.com>
2024-10-12 13:51:27 +0100

    migrating everythink to react toolkit

--- ReactRedux/src/components/Counter.js
+++ ReactRedux/src/components/Counter.js
@@ -1,21 +1,22 @@
 import classes from './Counter.module.css';
 import { useSelector, useDispatch } from 'react-redux';
+import { ActionStore } from '../store';
 const Counter = () => {
   const x = useSelector(state => state.counter);
   const show = useSelector(state => state.show)
   const dispatch = useDispatch()
   const toggleCounterHandler = () => {
-    dispatch({ type: "setShow" })
+    dispatch(ActionStore.Toggle())
   };
-const value = 50;
+  const value = 50;
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       {show && <div className={classes.value}>{x}</div>}
       <div>
-        <button onClick={() => { dispatch({ type: "incresby5", value: value }) }}>Incres By {value}</button>
-        <button onClick={() => { dispatch({ type: "decrement" }) }}>Decrement</button>
-        <button onClick={() => { dispatch({ type: "increment" }) }}>Increment</button>
+        <button onClick={() => { dispatch(ActionStore.Incres(100)) }}>Incres By {value}</button>
+        <button onClick={() => { dispatch(ActionStore.Decrement()) }}>Decrement</button>
+        <button onClick={() => { dispatch(ActionStore.Increment()) }}>Increment</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
--- ReactRedux/src/store/index.js
+++ ReactRedux/src/store/index.js
@@ -1,29 +1,31 @@
-import { createStore } from 'redux';
 import { createSlice } from '@reduxjs/toolkit'
 import { configureStore } from '@reduxjs/toolkit';
 
 const initialState = { counter: 0, show: true };
 
 const slideCounter = createSlice({
-    name: counter,
+    name: "counter",
     initialState,
     reducers: {
         Increment(state) {
-            state.counter++
+            state.counter++;
         },
         Decrement(state) {
-            state.counter--
+            state.counter--;
         },
         Incres(state, action) {
-            state.counter = state.counter + action.ammount
+            state.counter = state.counter + action.payload; // Correction ici
         },
-        Toggle() { }
-
+        Toggle(state) {
+            state.show = !state.show; // Ajout de la logique pour alterner l'état
+        }
     }
-})
+});
 
 const store = configureStore({
-    reducer: slideCounter
+    reducer: slideCounter.reducer
 });
+
+export const ActionStore = slideCounter.actions;
 
 export default store;
